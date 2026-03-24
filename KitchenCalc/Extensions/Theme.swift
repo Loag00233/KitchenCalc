46c7554f55b788c10f2ecf5520f1b56d815ca078
Loag00233 <33200goal@gmail.com>
2026-03-24 18:51:28 +0300

    Отредактировал структуру проекта

---
+++ KitchenCalc/Extensions/Theme.swift
@@ -0,0 +1,60 @@
+//
+//  Theme.swift
+//  KitchenCalc
+//
+//  Created by Ivan Ivashin on 24.03.2026.
+//
+
+import SwiftUI
+
+// MARK: - Colors
+extension Color {
+    
+    // Текст
+    static let textPrimary = Color.black.opacity(0.82)
+    static let textSecondary = Color.black.opacity(0.42)
+    static let textTertiary = Color.black.opacity(0.28)
+    
+    // Акцент (синий из конвертера)
+    static let accent = Color.accentColor
+    
+    // Градиент фона
+    static let gradientMint = Color(red: 0.70, green: 0.93, blue: 0.85)
+    static let gradientSky = Color(red: 0.55, green: 0.82, blue: 0.95)
+    static let gradientBlue = Color(red: 0.22, green: 0.50, blue: 0.85)
+    
+    // Готовый градиент как ShapeStyle
+    static let appBackground = LinearGradient(
+        colors: [.gradientMint, .gradientSky, .gradientBlue],
+        startPoint: .topLeading,
+        endPoint: .bottomTrailing
+    )
+}
+
+// MARK: - Typography
+extension Font {
+    static let productTitle = Font.system(size: 17, weight: .semibold)
+    static let productSubtitle = Font.system(size: 14)
+    static let converterValue = Font.system(size: 40, weight: .light)
+    static let unitLabel = Font.system(size: 15, weight: .medium)
+    static let sectionHeader = Font.system(size: 13, weight: .semibold)
+    static let bodyRegular = Font.system(size: 15)
+    static let historyArrow = Font.system(size: 11)
+    static let swapIcon = Font.system(size: 16, weight: .medium)
+    static let chevronSmall = Font.system(size: 10, weight: .semibold)
+    static let chevronMedium = Font.system(size: 13)
+}
+
+// MARK: - Spacing
+enum Spacing {
+    static let extraSmall: CGFloat = 4
+    static let small: CGFloat = 8
+    static let medium: CGFloat = 12
+    static let large: CGFloat = 16
+    static let extraLarge: CGFloat = 24
+}
+
+// MARK: - Radius
+enum Radius {
+    static let card: CGFloat = 20
+}
