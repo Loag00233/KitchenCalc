371cfdbad42c56d1183e0e0e0fcc155e3a828b32
Loag00233 <33200goal@gmail.com>
2026-03-31 11:26:22 +0300

    Добавил страницу кастомных мер и добавления новой меры

---
+++ KitchenCalc/Views/AddMeasureView.swift
@@ -0,0 +1,47 @@
+//
+//  AddMeasureView.swift
+//  KitchenCalc
+//
+//  Created by Ivan Ivashin on 31.03.2026.
+//
+
+import SwiftUI
+import SwiftData
+
+struct AddMeasureView: View {
+    
+    @State private var title: String = "title"
+    @State private var shortTitle: String = "shrt"
+    @State private var koefficient: Double = 0
+    @State private var isWeight: Bool = true
+    @State private var isCustom: Bool = true
+    
+    @Environment(\.modelContext) var modelContext
+    
+    
+    var body: some View {
+        TextField("Title", text: $title)
+        TextField("ShortTitle", text: $shortTitle)
+        TextField("Koefficient", value: $koefficient, format: .number)
+            .keyboardType(.decimalPad)
+        Picker("Type", selection: $isWeight) {
+              Text("Weight").tag(true)
+              Text("Volume").tag(false)
+          }
+          .pickerStyle(.segmented)
+        Button("Save") {
+            
+        }
+    }
+    
+    private func saveMeasure() {
+        guard !title.isEmpty, !shortTitle.isEmpty, !koefficient.isNaN else { return }
+        let newMeasure = Measure(title: title, shortTitle: shortTitle, koefficient: koefficient, isWeight: isWeight, isCustom: true)
+        modelContext.insert(newMeasure)
+        try? modelContext.save()
+    }
+}
+
+#Preview {
+    AddMeasureView()
+}
--- KitchenCalc/Views/CustomMeasuresView.swift
+++ KitchenCalc/Views/CustomMeasuresView.swift
@@ -6,16 +6,53 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct CustomMeasuresView: View {
     
-    @State private var showCustomMeasuresView = false
+    @State private var showAddSheet = false
+    @Environment(\.modelContext) var modelContext
+    @Query(filter: #Predicate<Measure> {$0.isCustom} ) var customMeasures: [Measure]
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Group {
+            if customMeasures.isEmpty {
+                ContentUnavailableView {
+                    Label("No custom measures", systemImage: "ruler")
+                } description: {
+                    Text("You haven't added any measures yet.")
+                } actions: {
+                    Button("Add New Measure") {
+                        showAddSheet = true
+                    }
+                }
+            } else {
+                List(customMeasures) { measure in
+                    HStack{
+                        Text(measure.displayTitle)
+                        Text("(\(measure.displayShortTitle))")
+                        Spacer()
+                        Text(measure.isWeight ? "Weight" : "Volume" )
+                    }
+                }
+                .toolbar{
+                    Button("Add") {showAddSheet = true }
+                }
+            }
+        }
+        .navigationTitle("List of your measures")
+        .sheet(isPresented: $showAddSheet) {
+            AddMeasureView()
+        }
+        
+        
+        
+        
     }
 }
 
 #Preview {
     CustomMeasuresView()
 }
+
