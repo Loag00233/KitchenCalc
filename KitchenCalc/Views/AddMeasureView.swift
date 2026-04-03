257ee78ced3b9505ffdb4a9c1df4af240dd89576
Loag00233 <33200goal@gmail.com>
2026-04-03 09:27:12 +0300

    отрефакторил AddMeasureView под MVVM

--- KitchenCalc/Views/AddMeasureView.swift
+++ KitchenCalc/Views/AddMeasureView.swift
@@ -6,43 +6,47 @@
 //
 
 import SwiftUI
-import SwiftData
 
 struct AddMeasureView: View {
     
-    @State private var title: String = "title"
-    @State private var shortTitle: String = "shrt"
+    @State private var title: String = ""
+    @State private var shortTitle: String = ""
     @State private var koefficient: Double = 0
     @State private var isWeight: Bool = true
-    @State private var isCustom: Bool = true
-    
-    @Environment(\.modelContext) var modelContext
+    @State private var showValidation = false
+    @Environment(CalcViewModel.self) private var viewModel
     @Environment(\.dismiss) var dismiss
     
     var body: some View {
-        TextField("Title", text: $title)
-        TextField("ShortTitle", text: $shortTitle)
-        TextField("Koefficient", value: $koefficient, format: .number)
-            .keyboardType(.decimalPad)
-        Picker("Type", selection: $isWeight) {
-              Text("Weight").tag(true)
-              Text("Volume").tag(false)
-          }
-          .pickerStyle(.segmented)
-        Button("Save") {
-            saveMeasure()
-            dismiss()
+        VStack {
+            TextField("Title", text: $title)
+                .modifier(TextFieldMod(isInvalid: showValidation && title.isEmpty))
+            TextField("ShortTitle", text: $shortTitle)
+                .modifier(TextFieldMod(isInvalid: showValidation && shortTitle.isEmpty))
+            TextField("Koefficient", value: $koefficient, format: .number)
+                .modifier(TextFieldMod(isInvalid: showValidation && koefficient <= 0 ))
+                .keyboardType(.decimalPad)
+            Picker("Type", selection: $isWeight) {
+                Text("Weight").tag(true)
+                Text("Volume").tag(false)
+            }
+            .pickerStyle(.segmented)
+            
+            Button("Save") {
+                guard viewModel.checkNewMeasureIsValid(title: title, shortTitle: shortTitle, koefficient: koefficient) else {
+                    showValidation = true
+                    return
+                }
+                viewModel.saveMeasure(title: title, shortTitle: shortTitle, koefficient: koefficient, isWeight: isWeight)
+                dismiss()
+            }
+            .modifier(ButtonMod(color: .blue))
+            if showValidation {
+                Text("Please fill in all fields")
+                    .foregroundStyle(.red)
+            }
         }
+        .padding(.horizontal, Spacing.medium)
     }
     
-    private func saveMeasure() {
-        guard !title.isEmpty, !shortTitle.isEmpty, !koefficient.isNaN else { return }
-        let newMeasure = Measure(title: title, shortTitle: shortTitle, koefficient: koefficient, isWeight: isWeight, isCustom: true)
-        modelContext.insert(newMeasure)
-        try? modelContext.save()
-    }
-}
-
-#Preview {
-    AddMeasureView()
 }
